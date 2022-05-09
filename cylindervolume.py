5403d1dc8b3297d77fe20c4c172910ab2734b6c0
Kumaravel V <75235334+Kumaravel655@users.noreply.github.com>
2022-05-09 18:55:44 +0530

    Add files via upload

---
+++ cylindervolume.py
@@ -0,0 +1,7 @@
+import math
+base = int(input("radious of base circle(r):"))
+height = int(input("height of the curve surface:"))
+
+volumeofcylinder=math.pi*pow(base,2)*height
+print("volume of a cylinder:",volumeofcylinder)
+
